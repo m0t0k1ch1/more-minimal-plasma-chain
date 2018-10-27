47bd10ec97e3755205ecf5350f581a92571e40ab
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-10-27 15:21:15 +0900

    add operator address to config

--- config.go
+++ config.go
@@ -1,5 +1,12 @@
 package main
 
+import "github.com/ethereum/go-ethereum/common"
+
 type Config struct {
-	Port int `json:"port"`
+	Port               int    `json:"port"`
+	OperatorAddressHex string `json:"operator_address_hex"`
+}
+
+func (conf *Config) OperatorAddress() common.Address {
+	return common.HexToAddress(conf.OperatorAddressHex)
 }
