6e6bd1016171afa970d23f70b700d89a26bfb9c9
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-10-30 18:31:01 +0900

    [core/types] enable to build Merkle tree at an arbitrary timing

--- core/types/block.go
+++ core/types/block.go
@@ -18,46 +18,31 @@ type BlockSummary struct {
 }
 
 type Block struct {
-	Txes       []*Tx
-	Number     uint64
-	Signature  Signature
-	merkleTree *merkle.Tree
+	Txes      []*Tx
+	Number    uint64
+	Signature Signature
 }
 
-func NewBlock(txes []*Tx, blkNum uint64) (*Block, error) {
-	blk := &Block{
-		Txes:       txes,
-		Number:     blkNum,
-		Signature:  nullSignature,
-		merkleTree: nil,
+func NewBlock(txes []*Tx, blkNum uint64) *Block {
+	return &Block{
+		Txes:      txes,
+		Number:    blkNum,
+		Signature: nullSignature,
 	}
-
-	if err := blk.initMerkleTree(); err != nil {
-		return nil, err
-	}
-
-	return blk, nil
 }
 
-func (blk *Block) initMerkleTree() error {
+func (blk *Block) MerkleTree() (*merkle.Tree, error) {
 	leaves := make([][]byte, len(blk.Txes))
 	for i, tx := range blk.Txes {
 		leaf, err := tx.MerkleLeaf()
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		leaves[i] = leaf
 	}
 
-	tree, err := merkle.NewTree(merkleConfig(), leaves)
-	if err != nil {
-		return err
-	}
-
-	blk.merkleTree = tree
-
-	return nil
+	return merkle.NewTree(merkleConfig(), leaves)
 }
 
 // implements RLP Encoder interface
@@ -77,8 +62,13 @@ func (blk *Block) Hash() ([]byte, error) {
 	return crypto.Keccak256(b), nil
 }
 
-func (blk *Block) Root() []byte {
-	return blk.merkleTree.Root().Bytes()
+func (blk *Block) Root() ([]byte, error) {
+	tree, err := blk.MerkleTree()
+	if err != nil {
+		return nil, err
+	}
+
+	return tree.Root().Bytes(), nil
 }
 
 func (blk *Block) Summary() (*BlockSummary, error) {
--- core/types/block_test.go
+++ core/types/block_test.go
@@ -10,62 +10,11 @@ import (
 )
 
 func newTestNullBlock(t *testing.T) *Block {
-	blk, err := NewBlock(nil, 0)
-	require.NoError(t, err)
-	return blk
+	return NewBlock(nil, 0)
 }
 
-func TestNewBlock(t *testing.T) {
-	type input struct {
-		txes   []*Tx
-		blkNum uint64
-	}
-	type output struct {
-		rootHex string
-		err     error
-	}
-	testCases := []struct {
-		name string
-		in   input
-		out  output
-	}{
-		{
-			"null block",
-			input{
-				nil,
-				0,
-			},
-			output{
-				"0xe026cc5a4aed3c22a58cbd3d2ac754c9352c5436f638042dca99034e83636516",
-				nil,
-			},
-		},
-		{
-			"deposit block",
-			input{
-				[]*Tx{newTestDepositTx(t)},
-				1,
-			},
-			output{
-				"0xf88f3819a6a679a60f8d5070af717bdfb41a87ab9eceb631136273928fb30560",
-				nil,
-			},
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			in, out := tc.in, tc.out
-
-			blk, err := NewBlock(in.txes, in.blkNum)
-			if out.err != nil {
-				assert.EqualError(t, err, out.err.Error())
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, out.rootHex, hexutil.Encode(blk.Root()))
-			}
-		})
-	}
+func newTestBlock(t *testing.T, txes []*Tx, blkNum uint64) *Block {
+	return NewBlock(txes, blkNum)
 }
 
 func TestBlock_Hash(t *testing.T) {
@@ -98,6 +47,49 @@ func TestBlock_Hash(t *testing.T) {
 			} else {
 				assert.NoError(t, err)
 				assert.Equal(t, out.hashHex, hexutil.Encode(hashBytes))
+			}
+		})
+	}
+}
+
+func TestBlock_Root(t *testing.T) {
+	type output struct {
+		rootHex string
+		err     error
+	}
+	testCases := []struct {
+		name string
+		blk  *Block
+		out  output
+	}{
+		{
+			"null block",
+			newTestNullBlock(t),
+			output{
+				"0xe026cc5a4aed3c22a58cbd3d2ac754c9352c5436f638042dca99034e83636516",
+				nil,
+			},
+		},
+		{
+			"deposit block",
+			newTestBlock(t, []*Tx{newTestDepositTx(t)}, 1),
+			output{
+				"0xf88f3819a6a679a60f8d5070af717bdfb41a87ab9eceb631136273928fb30560",
+				nil,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			blk, out := tc.blk, tc.out
+
+			rootBytes, err := blk.Root()
+			if out.err != nil {
+				assert.EqualError(t, err, out.err.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, out.rootHex, hexutil.Encode(rootBytes))
 			}
 		})
 	}
