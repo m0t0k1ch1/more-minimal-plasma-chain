84b56ef1c35ba7e534af69b7858d70068d3b1af7
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-11-01 16:01:02 +0900

    [app] align

--- app/handler_block.go
+++ app/handler_block.go
@@ -6,30 +6,6 @@ import (
 	"github.com/m0t0k1ch1/more-minimal-plasma-chain/utils"
 )
 
-func (cc *ChildChain) GetBlockHandler(c *Context) error {
-	blkHashBytes, err := c.GetBlockHashFromPath()
-	if err != nil {
-		return c.JSONError(err)
-	}
-
-	blk, err := cc.blockchain.GetBlock(blkHashBytes)
-	if err != nil {
-		if err == core.ErrBlockNotFound {
-			return c.JSONError(ErrBlockNotFound)
-		}
-		return c.JSONError(err)
-	}
-
-	blkBytes, err := rlp.EncodeToBytes(blk)
-	if err != nil {
-		return c.JSONError(err)
-	}
-
-	return c.JSONSuccess(map[string]interface{}{
-		"blk": utils.EncodeToHex(blkBytes),
-	})
-}
-
 func (cc *ChildChain) PostBlockHandler(c *Context) error {
 	c.Request().ParseForm()
 
@@ -76,5 +52,29 @@ func (cc *ChildChain) postDepositBlockHandler(c *Context) error {
 
 	return c.JSONSuccess(map[string]interface{}{
 		"blkhash": utils.EncodeToHex(blkHashBytes),
+	})
+}
+
+func (cc *ChildChain) GetBlockHandler(c *Context) error {
+	blkHashBytes, err := c.GetBlockHashFromPath()
+	if err != nil {
+		return c.JSONError(err)
+	}
+
+	blk, err := cc.blockchain.GetBlock(blkHashBytes)
+	if err != nil {
+		if err == core.ErrBlockNotFound {
+			return c.JSONError(ErrBlockNotFound)
+		}
+		return c.JSONError(err)
+	}
+
+	blkBytes, err := rlp.EncodeToBytes(blk)
+	if err != nil {
+		return c.JSONError(err)
+	}
+
+	return c.JSONSuccess(map[string]interface{}{
+		"blk": utils.EncodeToHex(blkBytes),
 	})
 }
--- app/handler_tx.go
+++ app/handler_tx.go
@@ -6,6 +6,33 @@ import (
 	"github.com/m0t0k1ch1/more-minimal-plasma-chain/utils"
 )
 
+func (cc *ChildChain) PostTxHandler(c *Context) error {
+	c.Request().ParseForm()
+
+	tx, err := c.GetTxFromForm()
+	if err != nil {
+		return c.JSONError(err)
+	}
+
+	txHashBytes, err := cc.blockchain.AddTxToMempool(tx)
+	if err != nil {
+		if err == core.ErrInvalidTxSignature {
+			return c.JSONError(ErrInvalidTxSignature)
+		} else if err == core.ErrInvalidTxBalance {
+			return c.JSONError(ErrInvalidTxBalance)
+		} else if err == core.ErrInvalidTxIn {
+			return c.JSONError(ErrInvalidTxIn)
+		} else if err == core.ErrTxOutAlreadySpent {
+			return c.JSONError(ErrTxOutAlreadySpent)
+		}
+		return c.JSONError(err)
+	}
+
+	return c.JSONSuccess(map[string]interface{}{
+		"txhash": utils.EncodeToHex(txHashBytes),
+	})
+}
+
 func (cc *ChildChain) GetTxHandler(c *Context) error {
 	txHashBytes, err := c.GetTxHashFromPath()
 	if err != nil {
@@ -46,33 +73,6 @@ func (cc *ChildChain) GetTxProofHandler(c *Context) error {
 
 	return c.JSONSuccess(map[string]interface{}{
 		"proof": utils.EncodeToHex(proofBytes),
-	})
-}
-
-func (cc *ChildChain) PostTxHandler(c *Context) error {
-	c.Request().ParseForm()
-
-	tx, err := c.GetTxFromForm()
-	if err != nil {
-		return c.JSONError(err)
-	}
-
-	txHashBytes, err := cc.blockchain.AddTxToMempool(tx)
-	if err != nil {
-		if err == core.ErrInvalidTxSignature {
-			return c.JSONError(ErrInvalidTxSignature)
-		} else if err == core.ErrInvalidTxBalance {
-			return c.JSONError(ErrInvalidTxBalance)
-		} else if err == core.ErrInvalidTxIn {
-			return c.JSONError(ErrInvalidTxIn)
-		} else if err == core.ErrTxOutAlreadySpent {
-			return c.JSONError(ErrTxOutAlreadySpent)
-		}
-		return c.JSONError(err)
-	}
-
-	return c.JSONSuccess(map[string]interface{}{
-		"txhash": utils.EncodeToHex(txHashBytes),
 	})
 }
 
