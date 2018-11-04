31f63de7369dfbff36656184e5a1c4de5814f1b4
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-11-04 18:15:23 +0900

    [utils] update funcs for hex

--- utils/hex.go
+++ utils/hex.go
@@ -1,43 +1,35 @@
 package utils
 
 import (
-	"fmt"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
-var (
-	NullHash = common.BytesToHash([]byte{
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00,
-	})
-)
-
-var (
-	ErrInvalidHashSize = fmt.Errorf("hash size must be %d bytes", common.HashLength)
-)
-
-func BytesToHash(b []byte) (common.Hash, error) {
-	if len(b) != common.HashLength {
-		return NullHash, ErrInvalidHashSize
-	}
-
-	h := common.Hash{}
-	copy(h[:], b[:])
-
-	return h, nil
+func IsHexAddress(s string) bool {
+	return common.IsHexAddress(s)
 }
 
-func HexToHash(s string) (common.Hash, error) {
-	b, err := DecodeHex(s)
-	if err != nil {
-		return NullHash, err
+func IsHexHash(s string) bool {
+	if hasHexPrefix(s) {
+		s = s[2:]
 	}
+	return len(s) == 2*common.HashLength && isHex(s)
+}
 
-	return BytesToHash(b)
+func BytesToAddress(b []byte) common.Address {
+	return common.BytesToAddress(b)
+}
+
+func BytesToHash(b []byte) common.Hash {
+	return common.BytesToHash(b)
+}
+
+func HexToAddress(s string) common.Address {
+	return common.HexToAddress(s)
+}
+
+func HexToHash(s string) common.Hash {
+	return common.HexToHash(s)
 }
 
 func AddressToHex(addr common.Address) string {
@@ -54,4 +46,24 @@ func DecodeHex(s string) ([]byte, error) {
 
 func EncodeToHex(b []byte) string {
 	return hexutil.Encode(b)
+}
+
+func hasHexPrefix(s string) bool {
+	return len(s) >= 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X')
+}
+
+func isHexCharacter(c byte) bool {
+	return ('0' <= c && c <= '9') || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F')
+}
+
+func isHex(s string) bool {
+	if len(s)%2 != 0 {
+		return false
+	}
+	for _, c := range []byte(s) {
+		if !isHexCharacter(c) {
+			return false
+		}
+	}
+	return true
 }
