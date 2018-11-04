8ec97963241fc04effddc3cc006a336146748a8a
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-11-04 15:51:06 +0900

    [utils] implement HashToHex()

--- utils/hex.go
+++ utils/hex.go
@@ -1,6 +1,13 @@
 package utils
 
-import "github.com/ethereum/go-ethereum/common/hexutil"
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
+
+func HashToHex(h common.Hash) string {
+	return EncodeToHex(h.Bytes())
+}
 
 func DecodeHex(s string) ([]byte, error) {
 	return hexutil.Decode(s)
