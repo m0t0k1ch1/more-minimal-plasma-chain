6d7f9b9f95652fc7f15cf2d812b1dace20621c5d
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-11-07 03:05:17 +0900

    [core] use types.MaxBlockTxesNum

--- core/position.go
+++ core/position.go
@@ -1,16 +1,24 @@
 package core
 
-import "math/big"
+import (
+	"math/big"
+
+	"github.com/m0t0k1ch1/more-minimal-plasma-chain/core/types"
+)
+
+var (
+	TxPositionOffset = big.NewInt(10000)
+)
 
 func TxPosition(blkNum, txIndex *big.Int) *big.Int {
-	pos := new(big.Int).Mul(blkNum, big.NewInt(100000))
+	pos := new(big.Int).Mul(blkNum, big.NewInt(types.MaxBlockTxesNum+1))
 	pos.Add(pos, txIndex)
 	return pos
 }
 
 func TxOutPosition(blkNum, txIndex, oIndex *big.Int) *big.Int {
 	pos := TxPosition(blkNum, txIndex)
-	pos.Mul(pos, big.NewInt(10000))
+	pos.Mul(pos, TxPositionOffset)
 	pos.Add(pos, oIndex)
 	return pos
 }
