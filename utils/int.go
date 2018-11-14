07589763a179f3e54d2418a463b726a6f4bb8923
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-11-15 01:45:17 +0900

    [utils] implement Uint64ToString() and StringToUint64()

--- utils/int.go
+++ utils/int.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"bytes"
 	"encoding/binary"
+	"strconv"
 )
 
 func Uint64ToBytes(i uint64) []byte {
@@ -11,6 +12,14 @@ func Uint64ToBytes(i uint64) []byte {
 	return b[:n]
 }
 
+func Uint64ToString(i uint64) string {
+	return strconv.FormatUint(i, 10)
+}
+
 func BytesToUint64(b []byte) (uint64, error) {
 	return binary.ReadUvarint(bytes.NewReader(b))
+}
+
+func StringToUint64(s string) (uint64, error) {
+	return strconv.ParseUint(s, 64, 10)
 }
